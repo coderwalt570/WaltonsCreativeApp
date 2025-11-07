af04948226053d427cd80c2549f43549dda3277f
coderwalt570 <160133763+coderwalt570@users.noreply.github.com>
2025-11-07 07:24:04 -0600

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,1 +1,23 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(403).json({ message: "Invalid or expired token" });
+  }
+};
+
+export const checkRole = (roles) => (req, res, next) => {
+  if (!roles.includes(req.user.role)) {
+    return res.status(403).json({ message: "Access denied" });
+  }
+  next();
+};
