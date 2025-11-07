463dbe897f9ee49bb175684ea1f379f1fcf821dc
coderwalt570 <160133763+coderwalt570@users.noreply.github.com>
2025-11-07 07:23:31 -0600

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,1 +1,25 @@
+import sql from "mssql/msnodesqlv8";
+import dotenv from "dotenv";
+dotenv.config();
 
+const dbConfig = {
+  server: process.env.DB_SERVER,
+  database: process.env.DB_DATABASE,
+  driver: process.env.DB_DRIVER,
+  options: {
+    trustedConnection: true
+  }
+};
+
+export const executeQuery = async (query, params = []) => {
+  try {
+    const pool = await sql.connect(dbConfig);
+    const request = pool.request();
+    params.forEach(p => request.input(p.name, p.type, p.value));
+    const result = await request.query(query);
+    return result.recordset;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
